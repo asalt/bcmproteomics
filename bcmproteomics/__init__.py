668a033578d0b92d2b092c2b670d268ce9ddaafe
Alex <a.saltzman920@gmail.com>
2021-10-18 13:33:25 -0500

    better file finding, remove unused imports, black format

--- bcmproteomics/__init__.py
+++ bcmproteomics/__init__.py
@@ -1,11 +1,11 @@
 from . import ispec
-from . import wrangler
-from . import classify
-from . import multicomparison as _multicomparison # module
-from .multicomparison import multicomparison
-from .plotting import plot_summary
+
+# from . import wrangler
+# from . import classify
+# from . import data_loading
+# from . import multicomparison as _multicomparison # module
+# from .multicomparison import multicomparison
+# from .plotting import plot_summary
 
 
-
-
-__version__ = '0.0.13'
+__version__ = "0.0.20"
